43f8021f056609365c75c390137a3d39fb2b5a74
Anas-AbuHasan <anasabuhasan665@gmail.com>
2025-02-01 20:54:35 +0200

    Add project files.

---
+++ Hammam_library/Models/Authors.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jenin_library.Models
+{
+    public class Authors
+    {
+        [Key]
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
